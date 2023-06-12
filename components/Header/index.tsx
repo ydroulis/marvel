beb518cb1b39be9a10a7a970a5f6c1ca64ff7bd2
ydroulis <lombardi.droulis@gmail.com>
2023-06-12 15:09:12 -0300

    change switch color

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
 import Switch from 'react-switch';
 import { useTheme } from 'styled-components';
+import { Inter } from 'next/font/google'
 import { Moon, Sun } from '@phosphor-icons/react';
 import * as S from './styles';
-import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,8 +26,8 @@ function Header({ toggleTheme }: HeaderProps) {
                     checkedIcon={false}
                     uncheckedIcon={false}
                     height={20}
-                    offColor={themeContext?.colors.switch}
-                    onColor={themeContext?.colors.switch}
+                    offColor='#7B7C81'
+                    onColor='#7B7C81'
                 />
                 <Sun size={32} />
             </S.ThemeSwitcher>
