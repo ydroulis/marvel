3841d7b59a48de89bc41ab9c0271bc1769126083
ydroulis <lombardi.droulis@gmail.com>
2023-06-13 19:06:00 -0300

    refac home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,20 @@
-import { useState } from 'react'
 import Head from 'next/head'
 import Header from '@/components/Header'
-import light from '@/styles/theme/light'
-import dark from '@/styles/theme/dark'
 import MainView from '@/components/MainView'
-import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global';
 
 
 export default function Home() {
-  const [theme, setTheme] = useState(light);
-
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light);
-  }
-
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <GlobalStyle />
       <Head>
         <title>Marvel App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Header toggleTheme={toggleTheme} />
+      <Header />
       <MainView />
-    </ThemeProvider>
+    </>
   )
 }
