27eb2ba1af6ce7caac7b343ec74f7ae132f1f4d0
ydroulis <lombardi.droulis@gmail.com>
2023-06-13 11:37:09 -0300

    refac search input

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -1,49 +1,13 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import React, { ChangeEvent } from 'react';
 import { Divider, IconButton, InputBase, Paper } from '@mui/material';
-import { MagnifyingGlass } from '@phosphor-icons/react';
-import { useTheme } from 'styled-components';
 import * as S from './styles';
 
-interface ICharacter {
-    name: string;
-    thumbnail: {
-        path: string;
-        extension: string
-    }
-}
-
 interface SearchInputProps {
-    characterList: ICharacter[] | undefined;
-    setFilteredCharacter: Dispatch<SetStateAction<ICharacter[] | undefined>>;
+    onChange: (evt: ChangeEvent<HTMLInputElement>) => void
+
 }
 
-const SearchInput: React.FC<SearchInputProps> = ({ characterList, setFilteredCharacter }) => {
-    const [text, setText] = useState('')
-    const [sugestions, setSugestions] = useState<ICharacter[]>()
-    const themeContext = useTheme();
-
-    const onChangeHandler = (text: string) => {
-        let matches: ICharacter[] | undefined = [];
-        if (text.length > 0) {
-            matches = characterList && characterList.filter(character => {
-                const regex = new RegExp(`${text}`, "gi");
-                return character.name.match(regex);
-            })
-        }
-        setSugestions(matches)
-        setText(text);
-    }
-
-    const onSugestionHandler = (text: string) => {
-        setText(text);
-        setSugestions([]);
-    }
-
-    const onClickSearchHandler = () => {
-        const charactersFiltered = characterList && characterList.filter((obj) => obj.name === text);
-        setFilteredCharacter(charactersFiltered)
-        setText('');
-    }
+const SearchInput: React.FC<SearchInputProps> = ({ onChange }) => {
 
     return (
         <S.InputContainer>
@@ -55,23 +19,10 @@ const SearchInput: React.FC<SearchInputProps> = ({ characterList, setFilteredCha
                     sx={{ ml: 1, flex: 1 }}
                     placeholder="Encontre seu herói preferido"
                     inputProps={{ 'aria-label': 'search google maps' }}
-                    onChange={(e) => onChangeHandler(e.target.value)}
-                    value={text}
+                    onChange={onChange}
                 />
-                <IconButton onClick={() => onClickSearchHandler()} type="button" sx={{ p: '10px' }} aria-label="search">
-                    <MagnifyingGlass size={32} color={themeContext?.colors.primary} />
-                </IconButton>
                 <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
             </Paper>
-            {sugestions && <div className='sugestionsList'>
-                {sugestions.map(item => {
-                    return (
-                        <div className='sugestion' onClick={() => onSugestionHandler(item.name)}>
-                            {item.name}
-                        </div>
-                    )
-                })}
-            </div>}
         </S.InputContainer>
     );
 }
