c0b0a540a60886c02e55846a11287c36191a3a2b
ydroulis <lombardi.droulis@gmail.com>
2023-06-13 07:46:24 -0300

    invert switch icons order

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import Image from 'next/image';
 import Switch from 'react-switch';
 import { useTheme } from 'styled-components';
-import { Inter } from 'next/font/google'
 import { Moon, Sun } from '@phosphor-icons/react';
 import * as S from './styles';
-
-const inter = Inter({ subsets: ['latin'] })
 
 interface HeaderProps {
     toggleTheme(): void;
@@ -16,20 +13,20 @@ const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
     const themeContext = useTheme();
 
     return (
-        <S.HeaderContainer className={inter.className}>
+        <S.HeaderContainer>
             <Image src='/images/Logo.png' width={105} height={45} alt='Marvel logo' />
             <S.ThemeSwitcher>
-                <Moon size={32} />
+                <Sun size={32} />
                 <Switch
                     onChange={toggleTheme}
-                    checked={themeContext?.title === 'light'}
+                    checked={themeContext?.title === 'dark'}
                     checkedIcon={false}
                     uncheckedIcon={false}
                     height={20}
                     offColor='#7B7C81'
                     onColor='#7B7C81'
                 />
-                <Sun size={32} />
+                <Moon size={32} />
             </S.ThemeSwitcher>
         </S.HeaderContainer>
     );
