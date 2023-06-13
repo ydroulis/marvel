f286ebbf33faea8e4de66cf24fd5553971b2622b
ydroulis <lombardi.droulis@gmail.com>
2023-06-13 19:04:14 -0300

    use context providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,43 @@
+import light from '@/styles/theme/light';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ThemeProvider } from 'styled-components';
+import ThemeHandlerContext from '../context/ThemeHandlerContext';
+import { DefaultTheme } from 'styled-components/dist/types';
+import CharacterContext from '@/context/CharacterContext';
+import { ICharacter } from '@/types/character';
 
 const queryClient = new QueryClient();
 
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState<DefaultTheme>(light);
+  const [character, setCharacter] = useState<ICharacter>({
+    name: '',
+    description: '',
+    id: 0,
+    comics: {
+      available: 0
+    },
+    events: {
+      available: 0
+    },
+    thumbnail: {
+      extension: '',
+      path: ''
+    }
+  });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <CharacterContext.Provider value={{ character, setCharacter }}>
+      <ThemeHandlerContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={theme}>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </ThemeProvider>
+      </ThemeHandlerContext.Provider>
+    </CharacterContext.Provider>
   )
 }
