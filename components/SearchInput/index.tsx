cbd7ab54ed3c6ca0ef60950c97e7212a58874c05
ydroulis <lombardi.droulis@gmail.com>
2023-06-13 07:49:19 -0300

    implement auto sugest and search behaviors

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import { Divider, IconButton, InputBase, Paper } from '@mui/material';
 import { MagnifyingGlass } from '@phosphor-icons/react';
 import { useTheme } from 'styled-components';
 import * as S from './styles';
 
-const SearchInput: React.FC = () => {
+interface ICharacter {
+    name: string;
+    thumbnail: {
+        path: string;
+        extension: string
+    }
+}
+
+interface SearchInputProps {
+    characterList: ICharacter[] | undefined;
+    setFilteredCharacter: Dispatch<SetStateAction<ICharacter[] | undefined>>;
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({ characterList, setFilteredCharacter }) => {
+    const [text, setText] = useState('')
+    const [sugestions, setSugestions] = useState<ICharacter[]>()
     const themeContext = useTheme();
+
+    const onChangeHandler = (text: string) => {
+        let matches: ICharacter[] | undefined = [];
+        if (text.length > 0) {
+            matches = characterList && characterList.filter(character => {
+                const regex = new RegExp(`${text}`, "gi");
+                return character.name.match(regex);
+            })
+        }
+        setSugestions(matches)
+        setText(text);
+    }
+
+    const onSugestionHandler = (text: string) => {
+        setText(text);
+        setSugestions([]);
+    }
+
+    const onClickSearchHandler = () => {
+        const charactersFiltered = characterList && characterList.filter((obj) => obj.name === text);
+        setFilteredCharacter(charactersFiltered)
+        setText('');
+    }
 
     return (
         <S.InputContainer>
             <Paper
-                onSubmit={() => { }}
                 component="form"
                 sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
             >
@@ -18,12 +55,23 @@ const SearchInput: React.FC = () => {
                     sx={{ ml: 1, flex: 1 }}
                     placeholder="Encontre seu herói preferido"
                     inputProps={{ 'aria-label': 'search google maps' }}
+                    onChange={(e) => onChangeHandler(e.target.value)}
+                    value={text}
                 />
-                <IconButton onClick={() => { }} type="button" sx={{ p: '10px' }} aria-label="search">
+                <IconButton onClick={() => onClickSearchHandler()} type="button" sx={{ p: '10px' }} aria-label="search">
                     <MagnifyingGlass size={32} color={themeContext?.colors.primary} />
                 </IconButton>
                 <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
             </Paper>
+            {sugestions && <div className='sugestionsList'>
+                {sugestions.map(item => {
+                    return (
+                        <div className='sugestion' onClick={() => onSugestionHandler(item.name)}>
+                            {item.name}
+                        </div>
+                    )
+                })}
+            </div>}
         </S.InputContainer>
     );
 }
--- components/SearchInput/styles.ts
+++ components/SearchInput/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const InputContainer = styled.div`
     width: 100%;
+    position: relative;
 
     form {
         width: 100%;
@@ -12,5 +13,32 @@ export const InputContainer = styled.div`
 
     input{
         color: ${props => props.theme.colors.primary};
+    }
+
+    .sugestionsList{
+        width: 100%;
+
+        position: absolute;
+        z-index: 999;
+
+        background: ${props => props.theme.colors.secondary};
+    }
+
+    .sugestion{
+        padding-left: 10px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 30px;
+
+        z-index: 9999999;
+
+        border-bottom: ${props => `1px ${props.theme.colors.primary} solid`};
+
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.3;
+        }
     }
 `
